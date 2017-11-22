04d7d18d9ef134c2af692ca065224f59202ca9a5
Manuel <m.a.c.p90@gmail.com>
2017-11-22 18:52:37 -0500

    routes creted

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,7 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import  {Routes} from' @angular/router'
 
+const routes: Routes = [
+ { path: '', redirectTo: 'home', pathMatch: 'full' },
+ { path: 'home',  component: my-home-componen },
+ { path: 'movie/:id', component: my-movie-component }
+];
 import { AppComponent } from './app.component';
 import { MyHomeComponenComponent } from './my-home-componen/my-home-componen.component';
 import { MyMovieComponentComponent } from './my-movie-component/my-movie-component.component';
@@ -22,3 +28,14 @@ import { MyMovieComponentComponent } from './my-movie-component/my-movie-compone
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'about', component: MyAboutComponent }
+];
